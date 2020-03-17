aea698d127ebc9a6cb57514ca24cb42841360c92
Asher Roper <roper@pdx.edu>
2020-03-16 17:42:18 -0700

    All posibilities for empty squares

--- sudoku.hs
+++ sudoku.hs
@@ -55,9 +55,20 @@ type Puzzle = [Row]
 -- Range from (0, 0) - (8, 8)
 type Coordinate = (Int, Int)
 
+-- A list of possible values for each blank cell
+type Possibilities = [(Coordinate, [Int])]
+
 ------------------------------------------------------------------------------
 -- Functions
 ------------------------------------------------------------------------------
+
+------------------------------------
+-- Helper Functions
+------------------------------------
+
+takeThree :: [int] -> [[int]]
+takeThree [] = []
+takeThree (a:b:c:ds) = [a,b,c] : takeThree ds
 
 ------------------------------------
 -- Altering the board
@@ -66,11 +77,16 @@ type Coordinate = (Int, Int)
 concatBoard :: Puzzle -> [Cell]
 concatBoard p = concat p
 
+combine :: [a] -> [a] -> [[a]]
+combine x y = [x,y]
+
 ------------------------------------
 -- Getting parts of the board
 ------------------------------------
 
--- Redundant, but put in for clarities sake
+-- List of list of every row in the puzzle
+-- Redundant since it is the same format as the puzzle,
+-- but put in for clarities sake
 rows :: Puzzle -> [Row]
 rows p = p 
 
@@ -78,12 +94,78 @@ rows p = p
 columns :: Puzzle -> [Column]
 columns p = transpose p 
 
---blocks :: Puzzle -> [Block]
---blocks p = (concatBoard p)
+blocks :: Puzzle -> [Block]
+blocks =  map concat . takeThree . concat . transpose . map takeThree 
 
 -- A list of all blank (initiallized to zero) cells in a puzzle
 blankCoordinates :: Puzzle -> [Coordinate]
-blankCoordinates  p = [(x, y) | x <- coordinateValues, y <- coordinateValues, p !! x !! y == 0]
+blankCoordinates  p = [(x, y) | x <- coordinateValues, y <- coordinateValues
+                                                     , p !! x !! y == 0]
+
+-- Gets row r from puzzle p
+-- r = row to get 
+getRow :: Int -> Puzzle -> Row
+getRow r p = (rows p) !! r
+
+-- Gets column c from puzzle p
+-- c = column to get 
+getColumn :: Int -> Puzzle -> Column
+getColumn c p = (columns p) !! c
+
+-- Gets block b from puzzle p
+-- b = block to get
+getBlock :: Coordinate -> Puzzle -> Block
+getBlock (row,col) p | (row < 3 && col < 3) = (blocks p) !! 0
+                     | (row < 6 && col < 3) = (blocks p) !! 1 
+                     | (row < 9 && col < 3) = (blocks p) !! 2
+                     | (row < 3 && col < 6) = (blocks p) !! 3
+                     | (row < 6 && col < 6) = (blocks p) !! 4
+                     | (row < 9 && col < 6) = (blocks p) !! 5
+                     | (row < 3 && col < 9) = (blocks p) !! 6
+                     | (row < 6 && col < 9) = (blocks p) !! 7
+                     | (row < 9 && col < 9) = (blocks p) !! 8
+
+-- Takes a list of blankCoordinates and outputs a list of list of all possbile values
+-- for each blank coordinate
+possibleCellValues :: [Coordinate] -> Puzzle -> [[Int]]
+possibleCellValues [] p = []
+possibleCellValues (x:xs) p = [i | i <- [1..9], checkAll i x p] : possibleCellValues xs p
+
+------------------------------------
+-- Checking for valid digits 
+------------------------------------
+
+-- Checks to see if an Int in some cell for some row is possible
+checkRow :: Int -> Row -> Bool
+checkRow c r = notElem c r 
+
+-- Checks to see if an Int in some cell for some column is possible
+checkColumn :: Int -> Column -> Bool
+checkColumn cell column = notElem cell column
+
+-- Checks to see if an Int in some cell for some block is possible
+checkBlock :: Int -> Block -> Bool
+checkBlock c b = notElem c b
+
+-- Returns true if integer i isn't in the row, column, or block of the coordinate
+checkAll :: Int -> Coordinate -> Puzzle -> Bool
+checkAll i (row,col) p = checkRow i (getRow row p)
+                      && checkColumn i (getColumn col p)
+                      && checkBlock i (getBlock (row,col) p)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
